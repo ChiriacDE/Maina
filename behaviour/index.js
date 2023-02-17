6b77b245ef181aff9dd509d40a619f64ef6b3eec
ChiriacDE <81509537+ChiriacDE@users.noreply.github.com>
2023-02-17 16:36:01 +0100

    Add files via upload
    Maina 1.1.3
    
    -> Footer z-index fixed
    -> Privacy and Terms pages added
    -> Nav links highlight function
    -> JS functions sorted in another file

--- behaviour/index.js
+++ behaviour/index.js
@@ -1,25 +1,5 @@
-// Get the last color so to fill automatically the wave color of the footer
-function getLastSectionBgColor() {
-    // Get all the section elements in the document
-    const sections = document.getElementsByTagName("section");
-    
-    // Get the last section element
-    const lastSection = sections[sections.length - 1];
-  
-    // Get the background color of the last section element
-    const bgColor = window.getComputedStyle(lastSection).getPropertyValue("background-color");
-    
-    return bgColor;
-}
-
-function changeFooterFillColor(color) {
-    // Get the element with the class name ".footer-wave-path"
-    const footerWavePath = document.querySelector(".footer-wave-path");
-  
-    // Set the fill color of the element to the specified color
-    footerWavePath.style.fill = color;
-}
-
 // Change automatically the fill color of the footer
 changeFooterFillColor(getLastSectionBgColor())
 
+// Highlight nav links based on users viewpoint
+highlightNavLinks()
